42fd407d5fa33ff1c97046aefc3c38719df453f1
Mackenzie  Adams <Zee@Mackenzies-iMac.local>
2016-04-14 14:21:48 -0400

    completed bash reefinery

---
+++ the_reefinery_bash_1/0-survival_kit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+touch unicorn
+echo "are pretty" >> unicorn
+mkdir castle
+mv unicorn castle
+rm -rf castle
---
+++ the_reefinery_bash_1/1-what-you-are-will-show-in-what-you-do.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ls /etc
+cat /etc/passwd
+echo ”May the fourth be with you" | awk '{print $4}'
